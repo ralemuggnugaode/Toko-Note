50595066e5cfbe9b77c0f22369f93fa3496b3b4b
grasselixir <grasselixir@gmail.com>
2026-07-30 10:16:47 +0700

    update-catatan-keluar

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,27 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\CatatanMasukController;
 use App\Http\Controllers\PengeluaranController;
 use App\Http\Controllers\stokBarangController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::name('page.')->group(function(){
-    Route::get('/',[PageController::class, 'index'])->name('home');
-});
+// Auth Routes (Tamu)
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::resource('stok-barang-719', stokBarangController::class);
-Route::resource('catatan-masuk-729', CatatanMasukController::class);
-Route::resource('catatan-keluar-742', CatatanKeluarController::class);
+// App Routes (Perlu Login)
+Route::middleware(['auth'])->group(function () {
+    Route::name('page.')->group(function(){
+        Route::get('/',[PageController::class, 'index'])->name('home');
+    });
+
+    Route::resource('stok-barang-719', stokBarangController::class);
+    Route::resource('catatan-masuk-729', CatatanMasukController::class);
+    Route::resource('catatan-keluar-742', CatatanKeluarController::class);
+});
